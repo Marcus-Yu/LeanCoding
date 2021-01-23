20ddaf0c69f088c7b50cef4e3f10d6a59502cfbc
MarcusYu88 <marcusyuca@gmail.com>
2021-01-23 20:19:37 +0000

    Play around strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+a = 'Hello'
+b = "Hello"
+c = """"Hello
+Next line
+3rd line 
+                      ----again
+    a little shorter!
+"""
+print ( c )
+name = input ("What is your name?\n:")
+print (c.upper())
+print (c.lower())
+print (c.title())
+print (c.capitalize())
+print (a * 10 )
