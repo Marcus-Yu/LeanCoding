ba6ec0ce518b5b63e1eaafddecfe2db71c7a80fb
MarcusYu88 <marcusyuca@gmail.com>
2021-03-06 00:28:13 +0000

    run button

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print ( "Welcome to my greeting program!" )
 name = input ("What is your name?")
 print ( "Hi", name, ", Nice to meet you!")
-input ( "Press Enter to exit!")
+input ( "Press Enter to exit!")--
